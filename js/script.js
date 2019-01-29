c35dbf7159a8ce08717989c3e412c81a3545e468
unknown <selim.baccouche@gmail.com>
2019-01-29 09:27:31 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(function(){
+     $('#contact-form').submit(function(e) {
+        e.preventDefault();
+        $('.comments').empty();
+        var postdata = $('#contact-form').serialize();
+        
+        $.ajax({
+            type: 'POST',
+            url: 'php/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                 
+                if(json.isSuccess) 
+                {
+                    $('#contact-form').append("<p class='thank-you'>Your message was correctly send thank you =)</p>");
+                    $('#contact-form')[0].reset();
+                }
+                else
+                {
+                    $('#firstname + .comments').html(json.firstnameError);
+                    $('#name + .comments').html(json.nameError);
+                    $('#email + .comments').html(json.emailError);
+                    $('#phone + .comments').html(json.phoneError);
+                    $('#message + .comments').html(json.messageError);
+                }                
+            }
+        });
+    });
+    $(".slidecontact").hide();
+    $(".headingcontact").click(function(){
+        $(".slidecontact").slideToggle('5000');
+
+
+});
+});
+$(document).ready(function() {
+$('.thumbnail').click(function(){
+      $('.modal-body').empty();
+    var title = $(this).parent('a').attr("title");
+    $('.modal-title').html(title);
+    $($(this).parents('div').html()).appendTo('.modal-body');
+    $('#myModal').modal({show:true});
+});
+});
